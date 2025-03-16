75cf0a384b0c8a2125a10ceb6b04c81829e73485
sidharthify <wednisegit@gmail.com>
2025-03-17 02:25:19 +0530

    Pacman: Define Screen Height & Width
    Change-Id: Id919e0982ea13483141f6ac484971ac15b58c337
    Signed-off-by: sidharthify <wednisegit@gmail.com>

---
+++ device.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/nothing/Pacman
+
+# Boot Animation
+TARGET_SCREEN_HEIGHT := 2412
+TARGET_SCREEN_WIDTH := 1080
