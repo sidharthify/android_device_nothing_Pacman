298fb1f48f1d47232d7d4bcb0972b608655cddfe
AntonioScordino <me@ntonino.eu>
2025-03-17 11:51:27 +0530

    Pacman: Set-up VINTF manifest(s)
    Change-Id: I0c7f180c5ddd5bbf5b9b6134e1114bcea2bf8458
    Signed-off-by: sidharthify <wednisegit@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -139,5 +139,11 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 TARGET_USES_PREBUILT_VENDOR_SEPOLICY := true
 TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR := true
 
+# VINTF
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml \
+    vendor/lineage/config/device_framework_matrix.xml
+
 # Treble
 BOARD_VNDK_VERSION := current
