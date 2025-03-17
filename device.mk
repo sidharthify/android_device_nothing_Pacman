bbefef379bb1f6869728d09a1f0524a66eecd597
erfanoabdi <erfangplus@gmail.com>
2025-03-17 12:53:30 +0530

    Pacman: Add A/B OTA Partitions
    Change-Id: I0764f98398969534909fd823b4b92780c0f301dd
    Signed-off-by: sidharthify <wednisegit@gmail.com>

--- device.mk
+++ device.mk
@@ -10,6 +10,48 @@ DEVICE_PATH := device/nothing/Pacman
 TARGET_SCREEN_HEIGHT := 2412
 TARGET_SCREEN_WIDTH := 1080
 
+# A/B
+AB_OTA_UPDATER := true
+AB_OTA_PARTITIONS += \
+    boot \
+    dtbo \
+    init_boot \
+    odm \
+    odm_dlkm \
+    product \
+    system \
+    system_ext \
+    system_dlkm  \
+    vendor \
+    vbmeta_vendor \
+    vbmeta_system \
+    vendor_boot \
+    vendor_dlkm
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=$(BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE) \
+    POSTINSTALL_OPTIONAL_system=true
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=$(BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE) \
+    POSTINSTALL_OPTIONAL_vendor=true
+
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.2-impl \
+    android.hardware.boot@1.2-impl.recovery \
+    android.hardware.boot@1.2-service
+
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_sideload \
+    update_verifier \
+    otapreopt_script \
+    checkpoint_gc
+
 # Audio
 $(call soong_config_set,android_hardware_audio,run_64bit,true)
 
